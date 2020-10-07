543e284436353fb3904d841896f9bf0ebab42fc7
mBourgesHC <matt@hirecalibre.com>
2020-10-07 17:08:08 +0200

    initial commit

---
+++ server.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+const AccessToken = require('twilio').jwt.AccessToken;
+const VideoGrant = AccessToken.VideoGrant;
+require('dotenv').config();
+
+const MAX_ALLOWED_SESSION_DURATION = 14400;
+const twilioAccountSid = process.env.TWILIO_ACCOUNT_SID;
+const twilioApiKeySID = process.env.TWILIO_API_KEY_SID;
+const twilioApiKeySecret = process.env.TWILIO_API_KEY_SECRET;
+const PORT = process.env.PORT || 3000;
+
+app.use(express.static(path.join(__dirname, 'build')));
+
+app.get('/token', (req, res) => {
+  const { identity, roomName } = req.query;
+  const token = new AccessToken(twilioAccountSid, twilioApiKeySID, twilioApiKeySecret, {
+    ttl: MAX_ALLOWED_SESSION_DURATION,
+  });
+  token.identity = identity;
+  const videoGrant = new VideoGrant({ room: roomName });
+  token.addGrant(videoGrant);
+
+  res.send(token.toJwt());
+
+  console.log(`issued token for ${identity} in room ${roomName}`);
+});
+
+app.get('*', (_, res) => res.sendFile(path.join(__dirname, 'pages/index.html')));
+
+app.listen(PORT, () => console.log(`token server running on ${PORT}`));
